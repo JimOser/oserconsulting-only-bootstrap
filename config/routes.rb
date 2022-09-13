62ed6dd29cdfd189756bec3ebe4f6fa4843ce309
Jim Oser <oserj@oserconsulting.com>
2022-09-13 23:03:26 +0000

    bootstrap is partially working. Shows two column layout of index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/deutsch', to: 'static#deutsch'
+  get '/treffpunkte', to: 'static#treffpunkte'
+  get '/espanol', to: 'static#español'
+  get '/español', to: 'static#español'
+  get '/posts', to: 'static#posts'
+  get '/roma', to: 'static#roma'
+  get '/lugaresdereunion', to: 'static#lugaresdereunión'
+  get '/ueber_mich', to: 'static#über_mich'
+  get '/kontakt', to: 'static#kontakt'
+  get '/impressum', to: 'static#impressum'
+  get '/article_20160724anfang', to: 'static#article_20160724anfang'
+  get '/article_20160725blogpost', to: 'static#article_20160725blogpost'
+  root 'static#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
