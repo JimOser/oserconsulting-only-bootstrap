b93bbaa3bf141412d5c9168372c218ccee7ed814
Jim Oser <oserj@oserconsulting.com>
2022-09-15 19:52:15 +0000

    SUPER IMPORTANT document.addEventListener("turbo:load",() =>

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,18 @@
 // Entry point for the build script in your package.json
+// import "@hotwired/turbo-rails"
+// import "./controllers"
+// import * as bootstrap from "bootstrap"
+
+// https://www.bootrails.com/blog/rails-7-bootstrap-5-tutorial/
+// Inside app/javascript/application.js
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+document.addEventListener("turbo:load",() =>{
+  let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+})
+
